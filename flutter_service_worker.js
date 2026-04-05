2c0c1b28a878886f59f93095b5ef164b23bab7ea
youssef-joe <youssef-joe@github.comgit config --global user.name youssef-joegit config --global user.email youssef-joe@github.com>
2026-04-06 00:53:25 +0200

    Deploy: force cache-bust with new service worker

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6b5f892db92dde9ea8c491a048ac811f
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "3040c60766f082593478415d150778f0",
+"flutter_bootstrap.js": "4cef14eee9cb62a52b5075708ef7d5f3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
