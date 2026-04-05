73a97d4483129f2112ebdbff45287317e78640d0
youssef-joe <youssef-joe@github.comgit config --global user.name youssef-joegit config --global user.email youssef-joe@github.com>
2026-04-06 01:24:25 +0200

    Deploy: save/restore scroll position fix - prevents jump on send while scrolled up

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,14 +46,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "6b5f892db92dde9ea8c491a048ac811f
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "4cef14eee9cb62a52b5075708ef7d5f3",
+"flutter_bootstrap.js": "f8742ac486a2de63cf938221374a1ecc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "56a8670cc558b8b468971cc6c6f8b309",
 "/": "56a8670cc558b8b468971cc6c6f8b309",
-"main.dart.js": "077a40fe1ea4725a44411484096ffe27",
+"main.dart.js": "eb6a16572694bc3fb94333116467177b",
 "manifest.json": "c49906769a36c62aac8e1c214534a7a0",
 "version.json": "de48428c5a1931c2f004093ac72ab6d5"};
 // The application shell files that are downloaded before a service worker can
